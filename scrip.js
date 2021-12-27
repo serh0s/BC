200b8ad2aeaafd5c2adcaf71ff17db0fb2c5873d
serh0s <89337200+serh0s@users.noreply.github.com>
2021-12-27 02:32:20 +0200

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
